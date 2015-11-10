74523d4a85719835e4a7c5af506c45bfe017e769
pthejasvi <pthejasv@andrew.cmu.edu>
2015-11-11 00:53:45 +1030

    V1.1
    Added create schema statement

--- database.sql
+++ database.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+CREATE SCHEMA hquiz;
+
+USE hquiz;
 --
 -- Table structure for table `answer_history`
 --
